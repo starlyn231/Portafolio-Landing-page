bf262d62c2e8c8899fae9500bfdf14f40128bda4
“Starlyn” <starlynfernandez1222@hotmail.com>
2022-08-27 22:06:45 -0400

    added enhahement to project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Banner } from './components/banner/Banner';
 import { Skills } from './components/skills/Skills';
 import { Projects } from './components/projects/Projects';
 import { Contact } from './components/contact/Contact';
+import { Footer } from './components/footer/Footer';
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -19,6 +19,31 @@ export const Contact = () => {
   const [status, setStatus] = useState({});
 
 
+  const onFormUpdate = (category, value) => {
+    setFormDetails({
+      ...formDetails,
+      [category]: value
+    })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText("Sending...");
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(formDetails),
+    });
+    setButtonText("Send");
+    let result = await response.json();
+    setFormDetails(formInitialDetails);
+    if (result.code == 200) {
+      setStatus({ succes: true, message: 'Message sent successfully' });
+    } else {
+      setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
+    }
+  }
 
 
   return (
